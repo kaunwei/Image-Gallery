3dc4b0d712c24ffbafb2d2b177f7e901a397ab4a
Ayesha <ayatf466@gmail.com>
2024-10-26 01:50:24 -0600

    Files Added

---
+++ Gallery/base/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Gallery(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField()
+    def __str__(self):
+        return self.title
